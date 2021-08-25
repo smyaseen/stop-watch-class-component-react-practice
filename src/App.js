aafd5fec3811cb0648bce036e602cb285dc1c7ca
smyaseen <smyaseen164@gmail.com>
2021-08-25 16:41:22 +0500

    implemented counter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,52 @@
-const App = () => {
-  return <h1>Stop Watch</h1>;
-};
+import React from "react";
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      timer: 0,
+      interval: () => {
+        return setInterval(() => {
+          this.setState({ timer: this.state.timer + 1 });
+        }, 1000);
+      },
+    };
+  }
+
+  startTimer = () => {
+    if (typeof this.state.interval === "number") return;
+    else if (typeof this.state.interval === "function")
+      this.setState({ interval: this.state.interval() });
+    else {
+      this.setState({
+        interval: setInterval(() => {
+          this.setState({ timer: this.state.timer + 1 });
+        }, 1000),
+      });
+    }
+  };
+
+  stopTimer = () => {
+    if (typeof this.state.interval !== "function") {
+      clearInterval(this.state.interval);
+      this.setState({ interval: undefined });
+    }
+  };
+
+  resetTimer = () => {
+    this.setState({ timer: 0 });
+  };
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.startTimer}>Start</button>{" "}
+        <button onClick={this.stopTimer}>Stop</button>{" "}
+        <button onClick={this.resetTimer}>Reset</button>
+        <h1>{this.state.timer}</h1>
+      </div>
+    );
+  }
+}
 
 export default App;
---
+++ src/Firebase.js
@@ -0,0 +1,15 @@
+import firebase from "firebase";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDRtlRN_jZQxezuaYc88ZVX7yr31XWX6yM",
+  authDomain: "stop-watch-31c6f.firebaseapp.com",
+  projectId: "stop-watch-31c6f",
+  storageBucket: "stop-watch-31c6f.appspot.com",
+  messagingSenderId: "61329579287",
+  appId: "1:61329579287:web:e9f9658d9e24a114a16b52",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
